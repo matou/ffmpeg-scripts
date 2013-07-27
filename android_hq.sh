9f1cd4f4384f8ff88d5da1494d4b6d92c9767cd1
Matthias Matousek <matthias.matousek@matou.de>
2013-07-27 14:42:09 +0200

    actually this is usually better, as it preserves the original value

--- android_hq.sh
+++ android_hq.sh
@@ -2,4 +2,4 @@
 
 # usage: $0 infile outfile
 
-ffmpeg -threads 8 -i $1 -sameq -vcodec mpeg4 -acodec aac -strict experimental -ac 2 -r 23 -ab 192k -aspect 16:9 $2
+ffmpeg -threads 8 -i $1 -sameq -vcodec mpeg4 -acodec aac -strict experimental -ac 2 -ab 192k -aspect 16:9 $2
