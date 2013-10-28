bf83b436b4e503b376a0d2da9758794a14cbdccc
Matthias Matousek <matthias.matousek@matou.de>
2013-10-28 23:22:35 +0100

    don't only encode the first minute

--- xbox360.sh
+++ xbox360.sh
@@ -2,4 +2,4 @@
 
 # usage: $0 infile outfile
 
-avconv -threads 8 -i $1 -t 60 -b 65536k -vcodec mpeg4 -profile 15 -acodec ac3 -ac 2 -ab 192k $2.mp4
+ffmpeg -threads 8 -i $1 -b 65536k -vcodec mpeg4 -profile 15 -acodec ac3 -ac 2 -ab 192k $2.mp4
