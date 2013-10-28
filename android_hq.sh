277ba15f6ec5e0439a0348ba9b7d820226a66ec0
Matthias Matousek <matthias.matousek@matou.de>
2013-10-28 23:05:14 +0100

    best quality

--- android_hq.sh
+++ android_hq.sh
@@ -2,4 +2,4 @@
 
 # usage: $0 infile outfile
 
-ffmpeg -threads 8 -i $1 -sameq -vcodec mpeg4 -acodec aac -strict experimental -ac 2 -ab 192k $2
+ffmpeg -threads 8 -i $1 -qscale 0 -vcodec mpeg4 -acodec aac -strict experimental -ac 2 -ab 192k $2
